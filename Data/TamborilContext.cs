28a41e6f3a3452dd9337aec9ba4ae88c7b08ecac
Ryan Silva <rdsp1@aluno.ifnmg.edu.br>
2022-06-22 10:23:14 -0300

    Criando a tabela de pagamentos no banco de dados

--- Data/TamborilContext.cs
+++ Data/TamborilContext.cs
@@ -8,5 +8,6 @@ public class TamborilContext : DbContext
   public TamborilContext(DbContextOptions<TamborilContext> options) : base(options) { }
   public DbSet<Customer> Customer { get; set; }
   public DbSet<Seller> Seller { get; set; }
+  public DbSet<Payment> Payment { get; set; }
 
 }
